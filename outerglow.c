67a4fd89f329168081af8122c51d32631883a5b1
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2023-06-30 19:32:54 +0000

    Add files via upload

--- outerglow.c
+++ outerglow.c
@@ -17,6 +17,20 @@
  * 2023 Beaver (GEGL Aura)
  */
 
+/*
+Graph of GEGL Aura to test without installing
+ 
+aura radius=24 seed=13   gradius=2 opacity=1.3 tile-size=10  
+mosaic tile-neatness=0 tile-type=triangles tile-size=14 seed=33 light-color=#00000000  joints-color=#00000000  tile-spacing=1
+opacity value=1.5
+oilify
+gaussian-blur std-dev-x=7 std-dev-y=7
+color-overlay value=#ffc400
+ */
+
+
+
+
 #include "config.h"
 #include <glib/gi18n-lib.h>
 
@@ -33,6 +47,7 @@ property_double (radius, _("Lens Blur to Glow"), 11.0)
   ui_range    (0.0, 50.0)
   ui_gamma    (2.0)
   ui_meta     ("unit", "pixel-distance")
+  ui_meta     ("sensitive", "! mosaic")
 
 
 property_double (gradius, _("Gaussian Blur to Glow"), 0.0)
@@ -43,47 +58,43 @@ property_double (gradius, _("Gaussian Blur to Glow"), 0.0)
   ui_meta     ("unit", "pixel-distance")
 
 
-property_double (opacity, _("Make Opacity over 100%"), 1)
-  value_range   (1, 4.0)
-  ui_steps      (1, 4.0)
-
-
-property_double (hopacity, _("hidden opacity"), 1.1)
-  value_range   (1, 4)
-  ui_steps      (1, 4)
-    ui_meta     ("role", "output-extent")
-
+property_double (opacity, _("Hyper Opacity"), 1.0)
+  value_range   (1.0, 4.0)
+  ui_steps      (1.0, 4.0)
 
 
 property_double (tile_size, _("Aura Size"), 3.8)
     description (_("Average diameter of each tile (in pixels)"))
     value_range (3.0, 16.0)
     ui_meta     ("unit", "pixel-distance")
+  ui_meta     ("sensitive", "! mosaic")
 
 property_double (tile_saturation, _("Gaps in Aura"), 5.4)
     description (_("Expand tiles by this amount"))
     value_range (3.0, 8.0)
-
+  ui_meta     ("sensitive", "! mosaic")
 
 property_int    (mask_radius, _("Internal Oilify filter for Aura"), 4)
     description (_("Radius of circle around pixel, can also be scaled per pixel by a buffer on the aux pad."))
     value_range (1, 7)
     ui_range (1, 7)
     ui_meta     ("unit", "pixel-distance")
+  ui_meta     ("sensitive", "! mosaic")
 
 property_seed (seed, _("Random seed for Aura"), rand)
 
-property_boolean (mosaic, _("Enable Firey Aura mode SLOW"), FALSE)
-  description    (_("Firey Aura mode via Mosaic Filter"))
+property_boolean (mosaic, _("Enable Base Mosaic"), FALSE)
+  description    (_("Base the aura off the Mosaic Filter"))
 
 
 
-property_double (gradius2, _("Firey Aura mode blur"), 6.0)
-  description (_("Blur radius"))
+property_double (gradius2, _("Base Mosaic mode blur"), 6.0)
+  description (_("Blur radius; shapes were intentionally left so this filter could have a hidden use of shape outlines."))
   value_range (0.0, G_MAXDOUBLE)
-  ui_range    (0.0, 50.0)
+  ui_range    (1.0, 50.0)
   ui_gamma    (2.0)
   ui_meta     ("unit", "pixel-distance")
+  ui_meta     ("sensitive", " mosaic")
 
 
 enum_start (gegl_mosaic_tile2)
@@ -93,33 +104,25 @@ enum_start (gegl_mosaic_tile2)
   enum_value (GEGL_MOSAIC_TILE_TRIANGLES2, "triangles", N_("Triangles"))
 enum_end (GeglMosaicTile2)
 
-property_enum (tile_type, _("Firey Aura Mode Flame shape"),
+property_enum (tile_type, _("Base Mosaic Mode Flame shape"),
     GeglMosaicTile2, gegl_mosaic_tile2, GEGL_MOSAIC_TILE_TRIANGLES2)
     description (_("What shape to use for tiles"))
+  ui_meta     ("sensitive", " mosaic")
 
-property_double (tile_size2, _("Firey Aura Mode Flame Size"), 40.0)
+property_double (tile_size2, _("Base Mosaic Mode Flame Size"), 40.0)
     description (_("Average diameter of each tile (in pixels)"))
     value_range (1.0, 70.0)
     ui_range    (5.0, 70.0)
     ui_meta     ("unit", "pixel-distance")
+  ui_meta     ("sensitive", " mosaic")
 
 
-property_double (tile_neatness, _("Tile neatness"), 0.0)
-    description (_("Deviation from perfectly formed tiles"))
-    value_range (0.0, 1.0)
-    ui_meta     ("role", "output-extent")
+
+property_seed (fireseed, _("Seed for Base Mosaic"), random)
+  ui_meta     ("sensitive", " mosaic")
 
 
-property_color (joints_color, _("Please make Transparent"), "#00000000")
-    ui_meta     ("role", "output-extent")
-
-
-property_color (light_color, _("Please make Transparent 2"), "#00000000")
-    ui_meta     ("role", "output-extent")
-
-property_seed (fireseed, _("Seed for Firey Aura"), random)
-
-/* I Need help. GEGL does not allow multiple "rand" commands to exist. Renaming it to a number doesn't work either. The goal is to get both Seeds to work. */
+/*  GEGL does not allow multiple "rand" commands to exist in this template but renaming it to random or perhaps anything seems to solve the issue. */
 
 
 
@@ -147,6 +150,7 @@ typedef struct
   GeglNode *gblur;
   GeglNode *mosaic;
   GeglNode *gblur2;
+  GeglNode *fixgraph;
   GeglNode *output;
 } State; 
 
@@ -156,7 +160,9 @@ static void attach (GeglOperation *operation)
 {
   GeglNode *gegl = operation->node;
   GeglProperties *o = GEGL_PROPERTIES (operation);
-  GeglNode *input, *output, *hopacity, *color, *oilify, *cubism, *xor, *gblur, *lblur, *nop, *mosaic, *gblur2, *opacity;
+  GeglNode *input, *output, *hopacity, *color, *oilify, *cubism, *xor, *fixgraph, *gblur, *lblur, *nop, *mosaic, *gblur2, *opacity;
+  GeglColor *hidden_color_aura = gegl_color_new ("#00000000");
+  GeglColor *hidden_color_aura2 = gegl_color_new ("#00000000");
 
 
   input    = gegl_node_get_input_proxy (gegl, "input");
@@ -168,7 +174,7 @@ static void attach (GeglOperation *operation)
                                   NULL);
 
   opacity   = gegl_node_new_child (gegl,
-                                  "operation", "gegl:opacity",
+                                  "operation", "gegl:opacity", "value", 1.1,
                                   NULL);
 
   hopacity   = gegl_node_new_child (gegl,
@@ -177,14 +183,13 @@ static void attach (GeglOperation *operation)
 
 
   mosaic   = gegl_node_new_child (gegl,
-                                  "operation", "gegl:mosaic",
-                                  NULL);
+                                  "operation", "gegl:mosaic", "tile-neatness", 0.0,
+ "joints-color", hidden_color_aura,
+ "light-color", hidden_color_aura2, NULL);
 
   gblur2   = gegl_node_new_child (gegl,
                                   "operation", "gegl:gaussian-blur",
                                   NULL);
-
-
 
 
   xor   = gegl_node_new_child (gegl,
@@ -211,13 +216,15 @@ static void attach (GeglOperation *operation)
                                   "operation", "gegl:gaussian-blur",
                                   NULL);
 
+  fixgraph = gegl_node_new_child (gegl,
+                                  "operation", "gegl:median-blur", "radius", 0,
+                                  NULL);
 
-
+/* Median Blur at 0 repairs a GEGL Graph from Opacity bug. This is for Gimp's non-destructive future */
 
   gegl_operation_meta_redirect (operation, "color", color, "value");
   gegl_operation_meta_redirect (operation, "radius", lblur, "radius");
   gegl_operation_meta_redirect (operation, "opacity", opacity, "value");
-  gegl_operation_meta_redirect (operation, "hopacity", hopacity, "value");
   gegl_operation_meta_redirect (operation, "tile_size", cubism, "tile-size");
   gegl_operation_meta_redirect (operation, "tile_saturation", cubism, "tile-saturation");
   gegl_operation_meta_redirect (operation, "mask_radius", oilify, "mask-radius");
@@ -228,17 +235,9 @@ static void attach (GeglOperation *operation)
   gegl_operation_meta_redirect (operation, "gradius2", gblur2, "std-dev-y");
   gegl_operation_meta_redirect (operation, "tile_type", mosaic, "tile-type");
   gegl_operation_meta_redirect (operation, "tile_size2", mosaic, "tile-size");
-  gegl_operation_meta_redirect (operation, "joints_color", mosaic, "joints-color");
-  gegl_operation_meta_redirect (operation, "light_color", mosaic, "light-color");
   gegl_operation_meta_redirect (operation, "fireseed", mosaic, "seed");
 
 
-
-
-
-
-  gegl_node_link_many (input, hopacity, nop, cubism, oilify, lblur, gblur, xor, color, opacity, output, NULL);
-  gegl_node_connect_from (xor, "aux", nop, "output");
 
 
 
@@ -258,6 +257,7 @@ static void attach (GeglOperation *operation)
   state->hopacity = hopacity;
   state->mosaic = mosaic;
   state->gblur2 = gblur2;
+  state->fixgraph = fixgraph;
   state->output = output;
   o->user_data = state;
 }
@@ -271,12 +271,12 @@ update_graph (GeglOperation *operation)
 
   if (o->mosaic)
   {
-    gegl_node_link_many (state->input, state->hopacity, state->nop, state->cubism, state->oilify, state->lblur, state->gblur, state->xor, state->color, state->color, state->opacity, state->mosaic, state->gblur2, state->output, NULL);
+    gegl_node_link_many (state->input, state->hopacity, state->nop, state->cubism, state->oilify, state->lblur, state->gblur, state->xor, state->color, state->opacity, state->mosaic, state->gblur2, state->fixgraph, state->output, NULL);
       gegl_node_connect_from (state->xor, "aux", state->nop, "output");
   }
   else
   {
-    gegl_node_link_many (state->input, state->hopacity, state->nop, state->cubism, state->oilify, state->lblur, state->gblur, state->xor, state->color, state->color, state->opacity, state->output, NULL);
+    gegl_node_link_many (state->input, state->hopacity, state->nop, state->cubism, state->oilify, state->lblur, state->gblur, state->xor, state->color,  state->opacity, state->fixgraph, state->output, NULL);
       gegl_node_connect_from (state->xor, "aux", state->nop, "output");
   }
 }
@@ -304,5 +304,5 @@ gegl_op_class_init (GeglOpClass *klass)
                      ""),
     NULL);
 }
-
+/* GEGL cannot replicate union with the master Gimp GEGL blend modes that all GEGL filters in Gimp have. Users have to manually change the layer space to union. */
 #endif
